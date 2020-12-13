2eeabe16fd0b554c6a3eb4015d8461be0e36c01b
Samip <samipgnyawali15@gmail.com>
2020-12-13 19:47:13 +0545

    first commit

---
+++ app/src/main/java/com/example/rateappdemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.rateappdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import hotchemi.android.rate.AppRate;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AppRate.with(this)
+                .setInstallDays(1)
+                .setLaunchTimes(3)
+                .setRemindInterval(2)
+                .monitor();
+        AppRate.showRateDialogIfMeetsConditions(this);
+        AppRate.with(this).showRateDialog(this);
+    }
+}
